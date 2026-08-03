16831a603615de858d34a9d82910b31ac8ccace5
nylyk <hi@nylyk.com>
2026-08-03 23:29:45 +0200

    add scaffolding

---
+++ crates/server/src/main.rs
@@ -0,0 +1,7 @@
+use tracing_subscriber::EnvFilter;
+
+fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
+        .init();
+}
